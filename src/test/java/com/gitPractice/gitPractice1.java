b5f87e0d67861e3a7b14c3f2990e0b9750e426e8
Ash24Win <106369109+Ash24Win@users.noreply.github.com>
2022-06-01 17:33:17 +0530

    Update gitPractice1.java

--- src/test/java/com/gitPractice/gitPractice1.java
+++ src/test/java/com/gitPractice/gitPractice1.java
@@ -2,11 +2,7 @@ package com.gitPractice;
 
 import org.testng.Reporter;
 import org.testng.annotations.Test;
-<<<<<<< HEAD
-// code by person 2 B
-=======
-//code by person 1 A
->>>>>>> f0d193fb961ed1a3b90a70f744cc399f28424451
+
 public class gitPractice1 {
 	@Test
 	public void gitBash()
