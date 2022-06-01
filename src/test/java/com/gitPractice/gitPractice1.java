3905fe4f0c3f9b771f13934331edbfdee834006c
Ash24Win <ashwinsomeshkubsad@gmail.com>
2022-06-01 12:29:48 +0530

    first commit

---
+++ src/test/java/com/gitPractice/gitPractice1.java
@@ -0,0 +1,13 @@
+package com.gitPractice;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class gitPractice1 {
+	@Test
+	public void gitBash()
+	{
+		Reporter.log("Hello Git",true);
+	}
+
+}
