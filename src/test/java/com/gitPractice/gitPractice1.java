faf0bcfe5fc926fce00eee8e054ccf3157cfaba9
Ash24Win <ashwinsomeshkubsad@gmail.com>
2022-06-01 16:45:31 +0530

    feuhf

--- src/test/java/com/gitPractice/gitPractice1.java
+++ src/test/java/com/gitPractice/gitPractice1.java
@@ -2,7 +2,7 @@ package com.gitPractice;
 
 import org.testng.Reporter;
 import org.testng.annotations.Test;
-// code by person 2 B
+
 public class gitPractice1 {
 	@Test
 	public void gitBash()
@@ -14,5 +14,5 @@ public class gitPractice1 {
 	{
 		Reporter.log("Hello Bash",true);
 	}
-
+// code by person 2 B
 }
